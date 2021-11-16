098ebe7ea9f9bdb1565404e00960246820defbb7
Gavin Grant <gavingrant@gmail.com>
2021-11-16 11:12:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# WavePortal 
+
+This is the frontend of a Web3 App built with Solidity and Ethereum smart contracts. This client can read and write data to the blockchain. The data in this app is messages users submit after connecting their wallet.
+
+## Live App
+
+See a live version of this app here: [WavePortal](https://waveportal.gavingrant.repl.co/).
+
+## WavePortal Smart Contract
+
+View the smart contract in this [GitHub repo](https://github.com/gavinmgrant/waveportal).
+
+## Technology
+
+* [JavaScript ES6](https://www.javascript.com/) - Programming language
+* [React](https://reactjs.org/) - JS library for UI
+* [Ethers](https://github.com/ethers-io/ethers.js) - Ethereum wallet implementation and utilities in JS
+* [MetaMask](https://metamask.io/) - Ethereum wallet to access functions on smart contract
---
+++ src/App.js
@@ -0,0 +1,221 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json"
+import './App.css';
+
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [message, setMessage] = useState("")
+  const [success, setSuccess] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [numWaves, setNumWaves] = useState(0);
+  const [allWaves, setAllWaves] = useState([]);
+
+  const contractAddress = "0x44dD0568Ae1b25C8E36c5183154745a7F615dcd0";
+  const contractABI = abi.abi;
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const waves = await wavePortalContract.getAllWaves();
+        const wavesCleaned = waves.map(wave => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
+
+        setAllWaves(wavesCleaned.reverse());
+        setNumWaves(wavesCleaned.length);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllWaves();
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  const wave = async () => {
+    setLoading(true);
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        setSuccess(true);
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        const waves = await wavePortalContract.getAllWaves().length;
+        setNumWaves(waves);
+        setLoading(false);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setSuccess(false);
+    }
+  };
+
+  useEffect(() => {
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log('NewWave', from, timestamp, message);
+      setAllWaves(prevState => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ].reverse());
+      setSuccess(false);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      wavePortalContract.on('NewWave', onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off('NewWave', onNewWave);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  useEffect(() => {
+    if (message.length > 2) setDisabled(false);
+  }, [message]);
+  
+  return (
+    <div className="mainContainer">
+      {numWaves ? (
+        <div className="number">
+          Number of messages sent to me: <span className="num">{numWaves}</span>
+        </div>
+      ) : null}
+
+      {success && (
+        <div className="thanks">
+          Thank you!
+        </div>
+      )}
+
+      <div className="dataContainer">
+        <div className="header">
+        Hey, I'm Gavin!
+        </div>
+
+        <div className="bio">
+          Connect your Ethereum wallet and send me a message!
+          To thank you for sending a message, 1 in 4 senders will win 0.0001 ETH.
+        
+          {currentAccount && (
+            <form className="message" onSubmit={wave}>
+              <input required className="input" type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Write your message here." />
+              
+              <input type="submit" className="waveButton" disabled={disabled} value={loading ? "Loading..." : "Send"} />
+            </form>
+          )}
+        </div>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+        {currentAccount && (
+          <>
+            <div className="bio">
+              Previously Sent Messages:
+            </div>
+            <div className="messages">
+              {allWaves.map((wave, index) => {
+                return (
+                  <div key={index} style={{ marginBottom: "8px" }}>
+                    <div>Address: {wave.address}</div>
+                    <div>Time: {wave.timestamp.toString()}</div>
+                    <div>Message: <b>{wave.message}</b></div>
+                  </div>)
+              })}
+            </div>
+          </>
+        )}
+      </div>
+
+      <footer className="photoCredit">
+        Follow Gavin on <a href="https://github.com/gavinmgrant/" target="_blank">GitHub</a> | Photo by <a href="https://unsplash.com/@grakozy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Greg Rakozy</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </footer>
+    </div>
+  );
+}
