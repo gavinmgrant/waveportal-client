301ffaf2ac3956833a62e6c8a9055b3bf9eb1eb3
Gavin Grant <gavingrant@gmail.com>
2021-11-16 20:45:23 -0800

    refactor: improve responsive design

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is the frontend of a Web3 App built with Solidity and Ethereum smart contra
 
 ## Live App
 
-See a live version of this app here: [WavePortal](https://waveportal.gavingrant.repl.co/).
+See a live version of this app here: [WavePortal](https://waveportal-client.vercel.app/).
 
 ## WavePortal Smart Contract
 
@@ -16,3 +16,6 @@ View the smart contract in this [GitHub repo](https://github.com/gavinmgrant/wav
 * [React](https://reactjs.org/) - JS library for UI
 * [Ethers](https://github.com/ethers-io/ethers.js) - Ethereum wallet implementation and utilities in JS
 * [MetaMask](https://metamask.io/) - Ethereum wallet to access functions on smart contract
+* [Vercel](https://vercel.com/) - Cloud platform for deployment
+
+This app was built as part of a [buildspace](https://buildspace.so/) project.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import abi from "./utils/WavePortal.json"
-import './App.css';
+import abi from "./utils/WavePortal.json";
+import "./App.css";
 
 export default function App() {
   const [loading, setLoading] = useState(false);
   const [disabled, setDisabled] = useState(true);
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
   const [success, setSuccess] = useState(false);
   const [currentAccount, setCurrentAccount] = useState("");
   const [numWaves, setNumWaves] = useState(0);
@@ -22,9 +22,13 @@ export default function App() {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
         const waves = await wavePortalContract.getAllWaves();
-        const wavesCleaned = waves.map(wave => {
+        const wavesCleaned = waves.map((wave) => {
           return {
             address: wave.waver,
             timestamp: new Date(wave.timestamp * 1000),
@@ -45,23 +49,23 @@ export default function App() {
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
-      
+
       if (!ethereum) {
         console.log("Make sure you have metamask!");
         return;
       } else {
         console.log("We have the ethereum object", ethereum);
       }
-      
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
-      
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account);
         getAllWaves();
       } else {
-        console.log("No authorized account found")
+        console.log("No authorized account found");
       }
     } catch (error) {
       console.log(error);
@@ -77,12 +81,14 @@ export default function App() {
         return;
       }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
       console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]); 
+      setCurrentAccount(accounts[0]);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
@@ -95,9 +101,15 @@ export default function App() {
         setSuccess(true);
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
 
-        const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 300000,
+        });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -121,15 +133,17 @@ export default function App() {
     let wavePortalContract;
 
     const onNewWave = (from, timestamp, message) => {
-      console.log('NewWave', from, timestamp, message);
-      setAllWaves(prevState => [
-        ...prevState,
-        {
-          address: from,
-          timestamp: new Date(timestamp * 1000),
-          message: message,
-        },
-      ].reverse());
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) =>
+        [
+          ...prevState,
+          {
+            address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message,
+          },
+        ].reverse()
+      );
       setSuccess(false);
     };
 
@@ -137,13 +151,17 @@ export default function App() {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
 
-      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-      wavePortalContract.on('NewWave', onNewWave);
+      wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      wavePortalContract.on("NewWave", onNewWave);
     }
 
     return () => {
       if (wavePortalContract) {
-        wavePortalContract.off('NewWave', onNewWave);
+        wavePortalContract.off("NewWave", onNewWave);
       }
     };
   }, []);
@@ -155,7 +173,7 @@ export default function App() {
   useEffect(() => {
     if (message.length > 2) setDisabled(false);
   }, [message]);
-  
+
   return (
     <div className="mainContainer">
       {numWaves ? (
@@ -164,26 +182,32 @@ export default function App() {
         </div>
       ) : null}
 
-      {success && (
-        <div className="thanks">
-          Thank you!
-        </div>
-      )}
+      {success && <div className="thanks">Thank you!</div>}
 
       <div className="dataContainer">
-        <div className="header">
-        Hey, I'm Gavin!
-        </div>
+        <div className="header">Hey, I'm Gavin!</div>
 
         <div className="bio">
-          Connect your Ethereum wallet and send me a message!
-          To thank you for sending a message, 1 in 4 senders will win 0.0001 ETH.
-        
+          Connect your Ethereum wallet and send me a message! To thank you for
+          sending a message, 1 in 4 senders will win 0.0001 ETH on the Rinkeby test network.
           {currentAccount && (
             <form className="message" onSubmit={wave}>
-              <input required className="input" type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Write your message here." />
-              
-              <input type="submit" className="waveButton" disabled={disabled} value={loading ? "Loading..." : "Send"} />
+              <input
+                required
+                className="input"
+                type="text"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Write your message here."
+              />
+
+              <input
+                type="submit"
+                className="waveButton"
+                disabled={disabled}
+                value={loading ? "Loading..." : "Send"}
+              />
             </form>
           )}
         </div>
@@ -196,17 +220,18 @@ export default function App() {
 
         {currentAccount && (
           <>
-            <div className="bio">
-              Previously Sent Messages:
-            </div>
+            <div className="bio">Previously Sent Messages:</div>
             <div className="messages">
               {allWaves.map((wave, index) => {
                 return (
                   <div key={index} style={{ marginBottom: "8px" }}>
                     <div>Address: {wave.address}</div>
                     <div>Time: {wave.timestamp.toString()}</div>
-                    <div>Message: <b>{wave.message}</b></div>
-                  </div>)
+                    <div>
+                      Message: <b>{wave.message}</b>
+                    </div>
+                  </div>
+                );
               })}
             </div>
           </>
@@ -214,7 +239,26 @@ export default function App() {
       </div>
 
       <footer className="photoCredit">
-        Follow Gavin on <a href="https://github.com/gavinmgrant/" target="_blank">GitHub</a> | Photo by <a href="https://unsplash.com/@grakozy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Greg Rakozy</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        <p>
+          Follow Gavin on{" "}
+          <a
+            href="https://github.com/gavinmgrant/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+        <p>
+          Photo by{" "}
+          <a href="https://unsplash.com/@grakozy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Greg Rakozy
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </p>
       </footer>
     </div>
   );
